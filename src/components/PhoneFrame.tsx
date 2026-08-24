df170a2daee9d7d1f01dfc18911ce6bdfe55e03d
Surya Teja <suryateja4104@gmail.com>
2026-08-24 17:04:22 +0530

    Update mobile layout zoom scaling to 1.8x as requested

--- src/components/PhoneFrame.tsx
+++ src/components/PhoneFrame.tsx
@@ -24,10 +24,10 @@ export const PhoneFrame: React.FC<PhoneFrameProps> = ({ children }) => {
           />
         </div>
 
-        {/* Zoomed mobile content container scaled 1.6x */}
+        {/* Zoomed mobile content container scaled 1.8x */}
         <div 
-          className="w-full max-w-[440px] min-h-screen flex flex-col relative z-10 pb-28"
-          style={{ zoom: 1.6 }}
+          className="w-full max-w-[460px] min-h-screen flex flex-col relative z-10 pb-28"
+          style={{ zoom: 1.8 }}
         >
           {children}
         </div>
